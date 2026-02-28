8de3e6de7b2a462580584c0ea3b812096113a369
Jose Vilca <vilcaterrazas@gmail.com>
2026-02-28 12:55:15 -0500

    Libreria v1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "pe.com.grupomoran" // Tu dominio
                 artifactId = "designsystem"     // Nombre de la librería
-                version = "1.0.12"               // Versión inicial
+                version = "1.0.13"               // Versión inicial
             }
         }
     }
